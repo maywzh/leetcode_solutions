2f429423ffa8cb60f2adbe31d1067f8f1fc15b4d
maywzh <maywzh@gmail.com>
2017-12-27 18:47:00 +0800

    autocommit

---
+++ 1115.交替打印-foo-bar.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1115 lang=java
+ *
+ * [1115] 交替打印FooBar
+ */
+
+// @lc code=start
+class FooBar {
+    private int n;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+        }
+    }
+}
+// @lc code=end
+
