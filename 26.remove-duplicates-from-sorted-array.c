945c3ef22cc52c509f250ea63eb7c8fb44f48816
maywzh <maywzh@gmail.com>
2019-01-01 10:22:48 +0800

    updates

--- 26.remove-duplicates-from-sorted-array.c
+++ 26.remove-duplicates-from-sorted-array.c
@@ -59,6 +59,16 @@
  * }
  * 
  */
-int removeDuplicates(int* nums, int numsSize) {
-    
+int removeDuplicates(int *nums, int numsSize)
+{
+    int i = 0, j = 0;
+    if (numsSize == 0)
+        return 0;
+    while (i < numsSize)
+    {
+        if (nums[j] != nums[i])
+            nums[++j] = nums[i];
+        i++;
+    }
+    return j + 1;
 }
