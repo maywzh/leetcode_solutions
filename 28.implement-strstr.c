c87a63ee45a974ae2d7688d84e9e309b93fc19b6
maywzh <maywzh@gmail.com>
2019-01-01 19:38:20 +0800

    new solutions

---
+++ 28.implement-strstr.c
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=28 lang=c
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (30.69%)
+ * Total Accepted:    354.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ */
+int strStr(char *haystack, char *needle)
+{
+    if (needle[0] == '\0')
+        return 0;
+    int i = 1, j = 0;
+    int *next = (int *)malloc(sizeof(int) * 100);
+    next[0] = -1;
+    while (needle[i] != '\0')
+    {
+        if ()
+    }
+}
