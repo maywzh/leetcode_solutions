019eaba8d7f09f90678d95ef6b2784c47932ae6f
Wangzhihui Mei <wangzhihui.mei@intel.com>
2024-10-14 10:30:20 +0800

    数学题

---
+++ 292.Nim Game.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=cpp
+ * @lcpr version=
+ *
+ * [292] Nim 游戏
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool canWinNim(int n) { return n % 4 != 0; }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+ */
